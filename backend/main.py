da4d573631b35364133289590a76007f22a7c853
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2026-06-06 12:26:46 +0530

    feat: initialize FastAPI application with CORS middleware and update ignore patterns

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Device Pulse",
+    description="Backend project for Device Pulse",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    author="Rachit",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Device Pulse API"}
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app= "main:app", host="0.0.0.0", port=8000, reload=True)
